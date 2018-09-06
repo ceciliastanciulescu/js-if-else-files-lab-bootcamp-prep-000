131ee5c4b75fb3e2690de12fe200c5a9aee7b5f2
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-09-06 07:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var name = "Joe"
 var height = "74"
 if(name==="Susan") {
+  console.log("Expected the name to be Susan")
+} else {
   console.log("The name is correct")
-  else if(name==="Susan") {
-    console.log("")
-  }
 }
 
 // Don't worry about this
