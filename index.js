7a9eac90359d0968aa3ee29d68d82e430d62c0f5
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-09-06 07:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ if(name==="Susan") {
 } else {
   console.log("The name is correct")
 }
-if()
+if(70+4===74) {
+  console.log("The height is correct")
+}
 // Don't worry about this
 module.exports = { name, height
 }
